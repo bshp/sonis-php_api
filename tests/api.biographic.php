97a5fc289c2bbcf5408626581d8570d47c65a917
Jason A Everling <jason.everling@gmail.com>
2019-08-13 12:02:18 -0500

    add tests for initial release

---
+++ tests/api.biographic.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 Jason A. Everling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ *    For Sonis Coldfusion Web Services
+ *
+ *    By: Jason A. Everling
+ *    Email: jason...@gmail.com
+ *
+ */
+
+require '../config.php';
+
+/**
+ * The method within the component
+ *
+ * @var $method
+ */
+$method = 'namesearch';
+
+/**
+ * Is data is returned?
+ *
+ * @var $returns
+ */
+$returns = 'yes';
+
+/**
+ * The object(s) primary key being retrieved
+ *
+ * @var $soc_sec
+ */
+$soc_sec = '000000000';
+
+/**
+ * All the arguments to send.
+ *
+ * Required format:
+ * $args = class::function(all your variables)
+ *
+ * @var mixed $args
+ */
+$args = biographic::namesearch($soc_sec);
+
+/**
+ * Start the API call process.
+ * Almost every call will look like this.
+ *
+ * @var mixed $request
+ */
+$request = $api->run($method, $returns, $args);
+
+print_r($request);
---
+++ tests/sql.sysvar.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 Jason A. Everling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ *    For Sonis Coldfusion Web Services
+ *
+ *    By: Jason A. Everling
+ *    Email: jason...@gmail.com
+ *
+ */
+
+require '../config.php';
+
+/**
+ * SOAP SQL is the easiest to work with.
+ *
+ * Data returned is exactly what you have
+ * requested, no more, no less. It is
+ * recommended to use the API over SQL
+ * to help guard against attacks.
+ *
+ */
+
+/**
+ * The query statement to be
+ * ran in regular sql language.
+ *
+ * @var string $sql
+ * @example "SELECT TOP 1 * FROM sysvar"
+ */
+$sql = "SELECT TOP 1 * FROM sysvar";
+
+/**
+ * Stores the data queried in $result.
+ * Data can be retrieved using the column
+ * name in all uppercase, COLUMN_NAME
+ *
+ * @var array $result
+ * @example $result['FIRST_NAME'];
+ */
+$result = $stmt->run($sql);
+
+print_r($result);
