b3b72c88d054e05cca4ee0c467cc5106bada54f3
Jason A Everling <jason.everling@gmail.com>
2019-09-27 17:49:43 -0500

    cosmetic formatting

--- sonis/lib/utils.php
+++ sonis/lib/utils.php
@@ -122,20 +122,28 @@ class utils
     public function utils_create_uuid($format = '', $case = '')
     {
         if ($format == 'cf') {
-            $uuid = sprintf('%04x%04x-%04x-%04x-%04x%04x%04x%04x',
-                mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            $uuid = sprintf(
+                '%04x%04x-%04x-%04x-%04x%04x%04x%04x',
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
                 mt_rand(0, 0xffff),
                 mt_rand(0, 0x0C2f) | 0x4000,
                 mt_rand(0, 0x3fff) | 0x8000,
-                mt_rand(0, 0x2Aff), mt_rand(0, 0xffD3), mt_rand(0, 0xff4B)
+                mt_rand(0, 0x2Aff),
+                mt_rand(0, 0xffD3),
+                mt_rand(0, 0xff4B)
             );
         } else {
-           $uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-                mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            $uuid = sprintf(
+                '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
                 mt_rand(0, 0xffff),
                 mt_rand(0, 0x0C2f) | 0x4000,
                 mt_rand(0, 0x3fff) | 0x8000,
-                mt_rand(0, 0x2Aff), mt_rand(0, 0xffD3), mt_rand(0, 0xff4B)
+                mt_rand(0, 0x2Aff),
+                mt_rand(0, 0xffD3),
+                mt_rand(0, 0xff4B)
             );
         }
         if ($case == 'uc') {
