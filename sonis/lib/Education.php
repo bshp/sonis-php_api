4bc081ac23edd8a88c0f7e44d91d4c911b9d63ab
Jason A Everling <jason.everling@gmail.com>
2019-09-30 09:58:05 -0500

    move back

---
+++ sonis/lib/Education.php
@@ -0,0 +1,383 @@
+<?php
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 Jason A. Everling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace Jenzabar\Sonis\Api;
+
+/**
+ * Class education
+ *
+ * Sonis API Framework
+ *
+ * Component: education.cfc
+ *
+ * @file Education.php
+ * @package Sonis API
+ * @author Jason A. Everling <jason...@gmail.com>
+ * @copyright 2016
+ * @license https://opensource.org/licenses/MIT
+ */
+class Education
+{
+
+    /**
+     * The Sonis component to call
+     *
+     * @var string $comp
+     */
+    private static $comp = 'education';
+
+    /**
+     * Deletes an education record
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $edu_rid Edu record id
+     * @return array
+     */
+    public static function deleteEducation($soc_sec, $edu_rid)
+    {
+        $comp = self::$comp;
+        $method = 'delete_education';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['edu_rid', $edu_rid]
+        ];
+        //return $params;
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Search for an education record
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $edu_rid Edu record id
+     * @return array
+     */
+    public static function eduSearch($soc_sec, $edu_rid = '')
+    {
+        $comp = self::$comp;
+        $method = 'eduSearch';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['edu_rid', $edu_rid],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Inserts a new edu record and sets as default
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $cohort_cod Cohort code
+     * @return array
+     */
+    public static function insertDefaultEducation($soc_sec, $cohort_cod)
+    {
+        $comp = self::$comp;
+        $method = 'insert_default_education';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['cohort_cod', $cohort_cod]
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Inserts a new edu record
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $educmem Educational record memo
+     * @param string $inst_cod Institutional code
+     * @param string $mod_stat The 2 character code for module status, such as ST, WD, AL, PS, FA, SF, and HI
+     * @param string $degree Degree for the given educational record
+     * @param string $enter_date Educational record entry date
+     * @param string $leav_date Educational record leaving date
+     * @param string $grad_mo Educational record gradfuation month
+     * @param string $grad_year Educational record gradfuation year
+     * @param string $graduated Educational record graduated, true or false
+     * @return array
+     */
+    public static function insertEducation(
+        $soc_sec,
+        $educmem,
+        $inst_cod,
+        $mod_stat,
+        $degree,
+        $enter_date,
+        $leav_date,
+        $grad_mo,
+        $grad_year,
+        $graduated
+    ) {
+        $comp = self::$comp;
+        $method = 'insert_education';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['inst_txt', $educmem],
+            ['inst_cod', $inst_cod],
+            ['mod_stat', $mod_stat],
+            ['degree', $degree],
+            ['enter_date', $enter_date],
+            ['leav_date', $leav_date],
+            ['grad_mo', $grad_mo],
+            ['grad_year', $grad_year],
+            ['graduated', $graduated]
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Inserts a new institution
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $inst_mem Institutional memo
+     * @param string $edu_rid Edu record id
+     * @return array
+     */
+    public static function insertInstitution($soc_sec, $inst_mem, $edu_rid)
+    {
+        $comp = self::$comp;
+        $method = 'insert_inst';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['inst_mem', $inst_mem],
+            ['edu_rid', $edu_rid],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Search for institution
+     *
+     * @param string $inst_state Institutional state
+     * @param string $inst_city Institutional city
+     * @param string $inst_txt Institutional description
+     * @param string $insttypcod Institutional type code
+     * @param string $inst_cntry Institutional country
+     * @return array
+     */
+    public static function searchInstitution($inst_state, $inst_city, $inst_txt, $insttypcod, $inst_cntry)
+    {
+        $comp = self::$comp;
+        $method = 'institutsearch';
+        $returns = 'yes';
+        $params = [
+            ['inst_city', $inst_city],
+            ['inst_cntry', $inst_cntry],
+            ['inst_state', $inst_state],
+            ['inst_txt', $inst_txt],
+            ['insttypcod', $insttypcod],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Update a given education record
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $edu_rid Edu record id
+     * @param string $eduinsttyp Institutional type
+     * @param string $degree Degree for the given educational record
+     * @param string $credits Credits
+     * @param string $qlty_pnts Quality point
+     * @param string $cqpa
+     * @param string $rankstud Student rank
+     * @param string $rankclass Class rank
+     * @param string $grad_date Graduation date
+     * @param string $enter_date Educational record entry date
+     * @param string $leav_date Educational record leaving date
+     * @param string $date_rec Date received
+     * @param string $matric_dt Matriculation date
+     * @param string $trans_rec Transcripts received
+     * @param string $prog_desc Program description
+     * @param string $educmem Educational record memo
+     * @param string $enr_age Enrollment age
+     * @param string $orig_grad Original graduation date
+     * @param string $grad_mo Educational record gradfuation month
+     * @param string $grad_year Educational record gradfuation year
+     * @param string $graduated Educational record graduated, true or false
+     * @param string $grad_honors Graduated with honors
+     * @param string $nmedu_ce1 Custom Field 1
+     * @param string $nmedu_le1 Custom Field 2
+     * @param string $nmedu_ne1 Custom Field 3
+     * @param string $inst_txt Institutional description
+     * @param string $cohort_cod Cohort code
+     * @param string $degree_sought Degree sought
+     * @param string $prg_sought Program sought
+     * @param string $ant_grad_date Anticipated graduation date
+     * @param string $gpa_creds GPA Credits
+     * @param string $transfered Transfered
+     * @param string $grad_stud
+     * @param string $OA_inprocess Online application in progress
+     * @param string $diploma Diploma
+     * @param string $employ_waiver_type Employer waiver type
+     * @param string $cur_cod
+     * @param string $prev_instname Previous instituion name
+     * @param string $grad_age Graduating age
+     * @param string $degreeclass_cod Degree class code
+     * @param string $nmedu_transmem Transfer memo
+     * @param string $employ_waiver Employer waiver
+     * @param string $override Override flag
+     * @return array
+     */
+    public static function updateEducation(
+        $soc_sec,
+        $edu_rid,
+        $eduinsttyp,
+        $degree,
+        $credits,
+        $qlty_pnts,
+        $cqpa,
+        $rankstud,
+        $rankclass,
+        $grad_date,
+        $enter_date,
+        $leav_date,
+        $date_rec,
+        $matric_dt,
+        $trans_rec,
+        $prog_desc,
+        $educmem,
+        $enr_age,
+        $orig_grad,
+        $grad_mo,
+        $grad_year,
+        $graduated,
+        $grad_honors,
+        $nmedu_ce1,
+        $nmedu_le1,
+        $nmedu_ne1,
+        $inst_txt,
+        $cohort_cod,
+        $degree_sought,
+        $prg_sought,
+        $ant_grad_date,
+        $gpa_creds,
+        $transfered,
+        $grad_stud,
+        $OA_inprocess,
+        $diploma,
+        $employ_waiver_type,
+        $cur_cod,
+        $prev_instname,
+        $grad_age,
+        $degreeclass_cod,
+        $nmedu_transmem,
+        $employ_waiver,
+        $override
+    ) {
+        $comp = self::$comp;
+        $method = 'update_education';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['edu_rid', $edu_rid],
+            ['eduinsttyp', $eduinsttyp],
+            ['degree', $degree],
+            ['credits', $credits],
+            ['qlty_pnts', $qlty_pnts],
+            ['cqpa', $cqpa],
+            ['rankstud', $rankstud],
+            ['rankclass', $rankclass],
+            ['grad_date', $grad_date],
+            ['enter_date', $enter_date],
+            ['leav_date', $leav_date],
+            ['date_rec', $date_rec],
+            ['matric_dt', $matric_dt],
+            ['trans_rec', $trans_rec],
+            ['prog_desc', $prog_desc],
+            ['educmem', $educmem],
+            ['enr_age', $enr_age],
+            ['orig_grad', $orig_grad],
+            ['grad_mo', $grad_mo],
+            ['grad_year', $grad_year],
+            ['graduated', $graduated],
+            ['grad_honors', $grad_honors],
+            ['nmedu_ce1', $nmedu_ce1],
+            ['nmedu_le1', $nmedu_le1],
+            ['nmedu_ne1', $nmedu_ne1],
+            ['inst_txt', $inst_txt],
+            ['cohort_cod', $cohort_cod],
+            ['degree_sought', $degree_sought],
+            ['prg_sought', $prg_sought],
+            ['ant_grad_date', $ant_grad_date],
+            ['gpa_creds', $gpa_creds],
+            ['transfered', $transfered],
+            ['grad_stud', $grad_stud],
+            ['OA_inprocess', $OA_inprocess],
+            ['diploma', $diploma],
+            ['employ_waiver_type', $employ_waiver_type],
+            ['cur_cod', $cur_cod],
+            ['prev_instname', $prev_instname],
+            ['grad_age', $grad_age],
+            ['degreeclass_cod', $degreeclass_cod],
+            ['nmedu_transmem', $nmedu_transmem],
+            ['employ_waiver', $employ_waiver],
+            ['override', $override],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+}
---
+++ sonis/lib/Program.php
@@ -0,0 +1,273 @@
+<?php
+/**
+ *
+ * MIT License
+ *
+ * Copyright (c) 2016 Jason A. Everling
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+namespace Jenzabar\Sonis\Api;
+
+/**
+ * Class program
+ *
+ * Sonis API Framework
+ *
+ * Component: program.cfc
+ *
+ * Usage: Program related, mainly for applicants
+ *
+ * @file Program.php
+ * @package Sonis API
+ * @author Jason A. Everling <jason...@gmail.com>
+ * @copyright 2016
+ * @license https://opensource.org/licenses/MIT
+ */
+class Program
+{
+
+    /**
+     * The Sonis component to call
+     *
+     * @var string $comp
+     */
+    private static $comp = 'program';
+
+    /**
+     * Search for a program
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @return array
+     */
+    public static function appProgramSearch($soc_sec)
+    {
+        $comp = self::$comp;
+        $method = 'approgsearch';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Complete an application
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @return array
+     */
+    public static function appComplete($soc_sec)
+    {
+        $comp = self::$comp;
+        $method = 'complete_app';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Delete a program record
+     *
+     * @param $app_rid
+     * @return array
+     */
+    public static function appProgramDelete($app_rid)
+    {
+        $comp = self::$comp;
+        $method = 'delete_approg';
+        $returns = 'yes';
+        $params = [
+            ['app_rid', $app_rid]
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Insert a program record
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $camp_cod The person's campus, in the form of the code
+     * @param string $entry_date
+     * @param string $prg_cod Program code
+     * @param string $div_cod The person's division, in the form of the code
+     * @param string $app_date
+     * @param string $ack_date
+     * @param string $trans_date
+     * @param string $is_applicant
+     * @param string $preferred The persons preferred address, yes or no
+     * @param string $incomplete
+     * @param string $app_rid
+     * @param string $ref_source
+     * @param string $fee_rec
+     * @param string $apfee_dt
+     * @param string $prior_app
+     * @param string $app_yr
+     * @param string $acknowledg
+     * @param string $sms_trans
+     * @param string $matric_fee
+     * @param string $degree_ap Degree applied
+     * @param string $degree_ac Degree accepted
+     * @param string $major_ap
+     * @param string $major_ac
+     * @param string $time_maint
+     * @param string $date_maint
+     * @param string $trans_done
+     * @param string $operator The persons unqiue ID adding or modifying the record. Please change the value
+     * @param string $d_soc_sec The objects unique identifier
+     * @return array
+     */
+    public static function insertAppProgram(
+        $soc_sec,
+        $camp_cod,
+        $entry_date,
+        $prg_cod,
+        $div_cod,
+        $app_date,
+        $ack_date,
+        $trans_date,
+        $is_applicant,
+        $preferred,
+        $incomplete,
+        $app_rid,
+        $ref_source,
+        $fee_rec,
+        $apfee_dt,
+        $prior_app,
+        $app_yr,
+        $acknowledg,
+        $sms_trans,
+        $matric_fee,
+        $degree_ap,
+        $degree_ac,
+        $major_ap,
+        $major_ac,
+        $time_maint,
+        $date_maint,
+        $trans_done,
+        $operator,
+        $d_soc_sec = ''
+    ) {
+        $comp = self::$comp;
+        $method = 'insert_approg';
+        $returns = 'yes';
+        $params = [
+            ['d_soc_sec', $d_soc_sec],
+            ['soc_sec', $soc_sec],
+            ['app_rid', $app_rid],
+            ['prg_cod', $prg_cod],
+            ['ref_source', $ref_source],
+            ['fee_rec', $fee_rec],
+            ['apfee_dt', $apfee_dt],
+            ['prior_app', $prior_app],
+            ['app_yr', $app_yr],
+            ['div_cod', $div_cod],
+            ['camp_cod', $camp_cod],
+            ['app_date', $app_date],
+            ['entry_date', $entry_date],
+            ['incomplete', $incomplete],
+            ['acknowledg', $acknowledg],
+            ['ack_date', $ack_date],
+            ['sms_trans', $sms_trans],
+            ['trans_date', $trans_date],
+            ['matric_fee', $matric_fee],
+            ['degree_ap', $degree_ap],
+            ['degree_ac', $degree_ac],
+            ['major_ap', $major_ap],
+            ['major_ac', $major_ac],
+            ['time_maint', $time_maint],
+            ['date_maint', $date_maint],
+            ['trans_done', $trans_done],
+            ['operator', $operator],
+            ['is_applicant', $is_applicant],
+            ['preferred', $preferred]
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Prevent repeats update
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @param string $prg_cod Program code
+     * @return array
+     */
+    public static function preventRepeats($soc_sec, $prg_cod)
+    {
+        $comp = self::$comp;
+        $method = 'preventrepeats';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+            ['prg_cod', $prg_cod],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+
+    /**
+     * Search for a program
+     *
+     * @param string $soc_sec The objects unique identifier
+     * @return array
+     */
+    public static function programSearch($soc_sec)
+    {
+        $comp = self::$comp;
+        $method = 'programSearch';
+        $returns = 'yes';
+        $params = [
+            ['soc_sec', $soc_sec],
+        ];
+        return [
+            'comp' => $comp,
+            'returns' => $returns,
+            'params' => $params,
+            'method' => $method
+        ];
+    }
+}
